736976a00ff42b58ee391371d737e448c22e583d
Ryan Alfred <nitradeufc@gmail.com>
2026-07-23 20:14:58 +0300

    Configure encrypted sync backend

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://llmbgigriltgikzmfmnf.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Ds1dKUcxOThu2jtXZFkcIQ_yRu6qKLm";
+export const ENCRYPTED_SYNC_TABLE = "journalpersonal_encrypted_sync";

b88c269930973bfb8b8539427a0d9098748f9edc
Ryan Alfred <nitradeufc@gmail.com>
2026-07-23 20:17:36 +0300

    Load account authentication and encrypted sync

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 
 import("./dashboard.js")
   .then(() => import("./resource-bootstrap.js"))
+  .then(() => import("./cloud-sync.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));

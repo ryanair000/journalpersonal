a1bff5a9f38cf686338d0e14d5a13489fb172288
Ryan Alfred <nitradeufc@gmail.com>
2026-07-23 19:32:17 +0300

    Keep dashboard source local

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
 
-import("https://cdn.jsdelivr.net/gh/ryanair000/journalpersonal@7ad06f262f3cbb99417d2c832fef976c7ea77272/script.js")
+import("./dashboard.js")
   .then(() => import("./resource-bootstrap.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));

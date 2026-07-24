8a2e9cfbd7e5c4554e506e81f72526e426090405
Ryan Alfred <nitradeufc@gmail.com>
2026-07-24 16:34:05 +0300

    Load body rhythm tracker

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ import("./dashboard.js")
   .then(() => import("./editing-actions.js"))
   .then(() => import("./state-write-guard.js"))
   .then(() => import("./settings.js"))
+  .then(() => import("./rhythm.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));

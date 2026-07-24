835ac870465a765735e8b64ab2ca0c76407df9b5
Ryan Alfred <nitradeufc@gmail.com>
2026-07-24 15:46:15 +0300

    Load accessible form interactions

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 
 import("./dashboard.js")
   .then(() => import("./resource-bootstrap.js"))
+  .then(() => import("./form-actions.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));

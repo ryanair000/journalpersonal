ca7e4c89f0245783108f081f65686fafa5ac2b2c
Ryan Alfred <nitradeufc@gmail.com>
2026-07-24 16:58:06 +0300

    Load the finance center module

--- script.js
+++ script.js
@@ -7,4 +7,5 @@ import("./dashboard.js")
   .then(() => import("./state-write-guard.js"))
   .then(() => import("./settings.js"))
   .then(() => import("./rhythm.js"))
+  .then(() => import("./finance.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));

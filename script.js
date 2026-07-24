05b742aca1f0de8c3ece4830877e37bbffa8a337
Ryan Alfred <nitradeufc@gmail.com>
2026-07-24 16:05:09 +0300

    Load editing and organisation module

--- script.js
+++ script.js
@@ -3,4 +3,5 @@
 import("./dashboard.js")
   .then(() => import("./resource-bootstrap.js"))
   .then(() => import("./form-actions.js"))
+  .then(() => import("./editing-actions.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));

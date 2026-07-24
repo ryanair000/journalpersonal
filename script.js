f2cfaba1f38debfb9d886d95ade5abaa6b0f6d0b
Ryan Alfred <nitradeufc@gmail.com>
2026-07-24 16:09:43 +0300

    Add editing and organisation controls
    * Add editing and organisation controls
    
    * Style editing and organisation controls
    
    * Load editing and organisation module
    
    * Preserve latest state during reload
    
    * Load state write guard

--- script.js
+++ script.js
@@ -3,4 +3,6 @@
 import("./dashboard.js")
   .then(() => import("./resource-bootstrap.js"))
   .then(() => import("./form-actions.js"))
+  .then(() => import("./editing-actions.js"))
+  .then(() => import("./state-write-guard.js"))
   .catch((error) => console.error("Unable to load dashboard modules.", error));
